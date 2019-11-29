ec1f3d1cbef1ec60e05f75f37b3abb59cae127d9
LeeJuOh <56071126+LeeJuOh@users.noreply.github.com>
2019-11-29 16:17:20 +0900

    최신버젼,,,
    최신버젼,,,

---
+++ AnGo_final_2/app/src/main/java/com/e/ango/Review/OnReviewItemClickListener.java
@@ -0,0 +1,8 @@
+package com.e.ango.Review;
+
+import android.view.View;
+
+public interface OnReviewItemClickListener {
+
+    public void onItemClick(UserReviewAdapter.ViewHolder holder, View view, int position);
+}
